48a27e0c5193987b77a1c88423578c5c76c7a278
Sam Mauldin <sam@fluidnode.com>
2015-04-17 18:41:57 -0500

    Transparent startup file

--- app/pages/files/public/computercraft/LuaGuard/definitions.lua
+++ app/pages/files/public/computercraft/LuaGuard/definitions.lua
@@ -5,7 +5,11 @@ viruses[1].name = "Test Virus"
 viruses[1].fragment = "Run this file to test that LuaGuard is installed and active."
 
 viruses[2] = {}
-viruses[2].name = "LiquidTech's Remote Access"
+viruses[2].name = "LiquidTech's Remote Access Virus Installer"
 viruses[2].fragment = "liqob.com/ccvirus"
+
+viruses[3] = {}
+viruses[3].name = "LiquidTech's Remote Access Virus Launcher"
+viruses[3].fragment = ".virus/nsh host"
 
 return viruses
