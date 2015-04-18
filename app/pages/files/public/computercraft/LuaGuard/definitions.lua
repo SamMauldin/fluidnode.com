60b2462cd2b54bbbab38df5cd5595f1d625b9fd5
Sam Mauldin <sam@fluidnode.com>
2015-04-17 19:09:41 -0500

    New definition

--- app/pages/files/public/computercraft/LuaGuard/definitions.lua
+++ app/pages/files/public/computercraft/LuaGuard/definitions.lua
@@ -5,11 +5,7 @@ viruses[1].name = "Test Virus"
 viruses[1].fragment = "Run this file to test that LuaGuard is installed and active."
 
 viruses[2] = {}
-viruses[2].name = "LiquidTech's Remote Access Virus Installer"
-viruses[2].fragment = "liqob.com/ccvirus"
-
-viruses[3] = {}
-viruses[3].name = "LiquidTech's Remote Access Virus Launcher"
-viruses[3].fragment = ".virus/nsh host"
+viruses[2].name = "LiquidTech's Remote Access Virus"
+viruses[2].fragment = "/.a/nsh host"
 
 return viruses
